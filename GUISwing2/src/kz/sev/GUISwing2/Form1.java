05140027648fab2d8d10d8911c12bb85a64994ed
HOME41 <HOME41@HOME41-PC>
2015-05-23 21:34:55 +0600

    доп. изм.

--- GUISwing2/src/kz/sev/GUISwing2/Form1.java
+++ GUISwing2/src/kz/sev/GUISwing2/Form1.java
@@ -22,17 +22,18 @@ import javax.swing.border.BevelBorder;
 
 public class Form1 extends JFrame
 {
-	JTextField t1;
+	private JTextField t1;
 	JButton b1;
 	JTable table;
 	JScrollPane scrollPane;
-	TableModel model;
 	
 	eHandler hendler = new eHandler();
 
 	public Form1(String s) 
 	{
 		super(s);
+		getContentPane().setLayout(null);
+		
 		
 		b1 = new JButton("Œ ");
 		b1.setBounds(10, 51, 89, 23);
@@ -42,27 +43,12 @@ public class Form1 extends JFrame
 		t1.setBounds(109, 52, 86, 20);
 		getContentPane().add(t1);
 		
-		
-		String[] columnNames = {
-			     "Name",
-			     "Last modified",
-			     "Type",
-			     "Size"
-			}; 
-		
-		String[][] data = {};
-	
+			
 		b1.addActionListener(hendler);
-		
-		
-		getContentPane().setLayout(null);
 		
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 96, 414, 249);
 		getContentPane().add(scrollPane);
-		
-		//table = new JTable(data,columnNames);
-		
 		
 		/*TableColumn column1 = new TableColumn();
         column1.setHeaderValue("id");
@@ -71,15 +57,17 @@ public class Form1 extends JFrame
         TableColumn column2 = new TableColumn();
         column2.setHeaderValue("name");
         table.addColumn(column2);*/
-        
-        model = new DefaultTableModel(data, columnNames);
-        
-        table = new JTable(model);
+		
 		/*LinkedList columnNames = new LinkedList<String>();
         columnNames.add("id");
         columnNames.add("name");*/
         
+		String[] columnNames = {"id","name","sale"}; 
+		String[][] data      = null;
 		
+		TableModel model = new DefaultTableModel(data, columnNames);
+        
+        table = new JTable(model);
 		table.setColumnSelectionAllowed(true);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		scrollPane.setViewportView(table);
@@ -118,9 +106,6 @@ public class Form1 extends JFrame
 		DefaultTableModel dtm = new DefaultTableModel();
 		dtm = (DefaultTableModel)t.getModel();
 		
-		//TableColumn column = new TableColumn();
-        //column.setHeaderValue("Name");
-		
 		for(r = 0; r < mas.length; r++)
 		{	
 			dtm.setRowCount(dtm.getRowCount() + 1);
@@ -129,17 +114,8 @@ public class Form1 extends JFrame
 			for(c = 0; c < mas[r].length; c++)
 			{
 				String z = Integer.toString(mas[r][c]);
-				//t[c]["id"] = z;
-				//model.setValueAt(z, r, c);
-				
-				
 				dtm.setValueAt(z, r, c);
-				//dtm.insertRow(r, mas);
-				//t.setValueAt(z, r, c);
-				
 			}
-			
-			//t.setModel(model);
 		
 			
 		}
